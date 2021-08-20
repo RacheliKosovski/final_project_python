322d3ac220d416fabe39ef31f294724467c08033
rivkigershon <r0556783842@gmail.com>
2021-08-20 10:43:46 +0300

    first_commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from Form import form
+import tkinter as tk
+from tkinter import ttk, messagebox, RAISED
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    form = form()
+
+
